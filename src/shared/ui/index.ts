1f7446fe1827ccf866f094f8a167b3d04a2f7269
ShotMeow <shotmeow@gmail.com>
2023-03-01 14:59:33 +0300

    Added: Dropdown Overlay

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,3 +2,4 @@ export { Button } from "./Button/Button";
 export { Logo } from "./Logo/Logo";
 export { Burger } from "./Icons/Burger/Burger";
 export { Navigation } from "./Navigation/Navigation";
+export { Dropdown } from "./Dropdown/Dropdown";
