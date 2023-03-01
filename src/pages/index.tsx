6535adf5f351478999461b2e9c8c837f22bc7ba8
ShotMeow <shotmeow@gmail.com>
2023-03-01 16:02:10 +0300

    Added: Advantages Section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { NextPage } from "next";
 import Head from "next/head";
 import { Layout } from "@/shared/ui";
-import { Landing } from "@/widgets/landing";
+import { LandingSection } from "@/widgets/landing";
+import { AdvantagesSection } from "@/widgets/advantages/ui/AdvantagesSection/AdvantagesSection";
 
 const HomePage: NextPage = () => {
   return (
@@ -10,7 +11,8 @@ const HomePage: NextPage = () => {
       <Head>
         <title>Cloud Skinchanger</title>
       </Head>
-      <Landing />
+      <LandingSection />
+      <AdvantagesSection />
     </Layout>
   );
 };
